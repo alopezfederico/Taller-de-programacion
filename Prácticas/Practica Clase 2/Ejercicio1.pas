30c8ab61fad9049d3f98b3c40f475aca5c61c042
Fede Notebook <flopex91@gmail.com>
2024-08-23 21:51:37 -0300

    Practica Clase 2 arrancado

---
+++ Prácticas/Practica Clase 2/Ejercicio1.pas
@@ -0,0 +1,28 @@
+{1.- Descargar el programa ImperativoEjercicioClase2.pas que contiene parte del código del
+siguiente enunciado a resolver. Implementar lo indicado en el código.
+Implementar un programa que invoque a los siguientes módulos.
+
+a. Un módulo recursivo que retorne un vector de a lo sumo 15 números enteros “random”
+mayores a 10 y menores a 155 (incluidos ambos). La carga finaliza con el valor 20.
+
+b. Un módulo no recursivo que reciba el vector generado en a) e imprima el contenido del
+vector.
+
+c. Un módulo recursivo que reciba el vector generado en a) e imprima el contenido del vector.
+
+d. Un módulo recursivo que reciba el vector generado en a) y devuelva la suma de los valores
+pares contenidos en el vector.
+
+e. Un módulo recursivo que reciba el vector generado en a) y devuelva el máximo valor del
+vector.
+
+f. Un módulo recursivo que reciba el vector generado en a) y un valor y devuelva verdadero si
+dicho valor se encuentra en el vector o falso en caso contrario.
+
+g. Un módulo que reciba el vector generado en a) e imprima, para cada número contenido en
+el vector, sus dígitos en el orden en que aparecen en el número. Debe implementarse un
+módulo recursivo que reciba el número e imprima lo pedido. Ejemplo si se lee el valor 142, se
+debe imprimir 1 4 2}
+
+program Ejercicio1;
+
