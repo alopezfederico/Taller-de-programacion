6e594ec576dcb448f24f6a3cc8804c59927726be
Fede Notebook <flopex91@gmail.com>
2024-09-25 20:08:54 -0300

    Resumen variables

---
+++ Modulo2/Proyectos/Proyecto Clase 1/src/proyecto/clase/pkg1/ProyectoClase1.java
@@ -0,0 +1,13 @@
+package proyectoclase1;
+
+public class ProyectoClase1 {
+
+    public static void main (String[] args) {
+        String nombreDelAlumno;
+        boolean esArgentino = true;
+
+        nombreDelAlumno = "Federico";
+        nombreDelAlumno = nombreDelAlumno + "Lopez";
+
+    }
+}
